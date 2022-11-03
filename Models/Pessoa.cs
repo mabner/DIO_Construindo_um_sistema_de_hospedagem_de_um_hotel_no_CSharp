40df6ec0443fb7ecb905c4c2e01ae72f86654d4f
Marcos Leite <m.abner@live.co.uk>
2022-11-03 14:05:27 -0300

    change spaces for tabs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -2,20 +2,20 @@ namespace DesafioProjetoHospedagem.Models;
 
 public class Pessoa
 {
-    public Pessoa() { }
+	public Pessoa() { }
 
-    public Pessoa(string nome)
-    {
-        Nome = nome;
-    }
+	public Pessoa(string nome)
+	{
+		Nome = nome;
+	}
 
-    public Pessoa(string nome, string sobrenome)
-    {
-        Nome = nome;
-        Sobrenome = sobrenome;
-    }
+	public Pessoa(string nome, string sobrenome)
+	{
+		Nome = nome;
+		Sobrenome = sobrenome;
+	}
 
-    public string Nome { get; set; }
-    public string Sobrenome { get; set; }
-    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+	public string Nome { get; set; }
+	public string Sobrenome { get; set; }
+	public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
 }
