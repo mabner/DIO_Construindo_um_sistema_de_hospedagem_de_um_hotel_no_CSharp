d7615bbe66721bcf22b5042262103919f2b39684
Marcos Leite <m.abner@live.co.uk>
2022-11-03 16:08:16 -0300

    cadastro de hospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
 		{
 			// TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
 			// *IMPLEMENTE AQUI*
-			if (true)
+			if (Suite.Capacidade <= hospedes.Count)
 			{
 				Hospedes = hospedes;
 			}
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
 			{
 				// TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
 				// *IMPLEMENTE AQUI*
+				Console.WriteLine($"Capacidade do quarto não pode exceder {Suite.Capacidade}");
 			}
 		}
 
